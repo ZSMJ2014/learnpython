2679f7629b4b0172d9871c233760691b4370a8d1
liuang <liuang1990@163.com>
2015-12-09 17:39:55 +0800

    complete orm

--- awesome-python3-webapp/www/orm.py
+++ awesome-python3-webapp/www/orm.py
@@ -129,4 +129,84 @@ class ModelMetaclass(type):
         attrs['__update__']='update `%s` set %s where `%s`=?'%(tableName,','.join(map(lambda f:'`%s`=?'%(mappings.get(f).name or f),fields)),primaryKey)
         attrs['__delete__']='delete from `%s` where `%s`=?'%(tableName,primaryKey)
         return type.__new__(cls,name,bases,attrs)
-
+class Model(dict,metaclass=ModelMetaclass):
+    """docstring for Model"""
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+    def __getattr__(self,key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'"%key)
+    def __setattr__(self,key,value):
+        self[key]=value
+    def getValue(self,key):
+        return getattr(self,key,None)
+    def getValueOrDefault(self,key):
+        value=getattr(self,key,None)
+        if value is None:
+            field=self.__mappings__[key]
+            if field.default is not None:
+                value=field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s:%s'%(key,str(value)))
+                setattr(self,key,value)
+        return value
+    @classmethod
+    @asyncio.coroutine
+    def findAll(cls,where=None,args=None,**kw):
+        'find objects by where clause'
+        sql=[cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args=[]
+        orderBy=kw.get('orderBy',None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+        limit=kw.get('limit',None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit,int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit,tuple) and len(limit)==2:
+                sql.append('?,?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value:%s'%str(limit))
+        rs=yield from select(' '.join(sql),args)
+        return [cls(**r) for r in rs]
+    @classmethod
+    @asyncio.coroutine
+    def findNumber(cls,selectField,where=None,args=None):
+        'find number by select and where'
+        sql=['select %s _num_ from `%s`'%(selectField,cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs=yield from select(' '.join(sql),args,1)
+        if len(rs)==0:
+            return None
+        return rs[0]['_num_']
+    @asyncio.coroutine
+    def save(self):
+        args=list(map(self.getValueOrDefault,self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows=yield from execute(self.__insert__,args)
+        if rows!=1:
+            logging.warn('failed to insert record:affected rows:%s'%rows)
+    @asyncio.coroutine
+    def update(self):
+        args=list(map(self.getValue,self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows=yield from execute(self.__update__,args)
+        if rows!=1:
+            logging.warn('failed to update by primary key:affected rows:%s'%rows)
+    @asyncio.coroutine
+    def remove(self):
+        args=[self.getValue(self.__primary_key__)]
+        rows=yield from execute(self.__delete__,args)
+        if rows!=1:
+            logging.warn('failed to remove by primary key:affected rows:%s'%rows)
