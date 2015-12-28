06c590b9120e96d667ce8ba09eb6fb5ec2eebec8
liuang <liuang@cnic.cn>
2015-12-28 21:53:20 +0800

    register

--- awesome-python3-webapp/www/handlers.py
+++ awesome-python3-webapp/www/handlers.py
@@ -119,4 +119,17 @@ def api_register_users(*,email,name,passwd):
     if not passwd or not _RE_SHA1.match(passwd):
         raise APIValueError('passwd')
     users=yield from User.findAll('email=?',[email])
+    if len(users)>0:
+        raise APIValueError('register:failed','email','Email is already in use.')
+    uid=next_id()
+    sha1_passwd='%s:%s'%(uid,passwd)
+    user=User(id=uid,name=name.strip(),email=email,passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),image='http://www.gravatar.com/avatar/%s?d=mm&s=120'%hashlib.md5(email.encode('utf-8')).hexdigest())
+    yield from user.save()
+    #make session cookie:
+    r=web.Response()
+    r.set_cookie(COOKIE_NAME,user2cookie(user,86400),httponly=True)
+    user.passwd='******'
+    r.content_type='application/json'
+    r.body=json.dumps(user,ensure_ascii=False).encode('utf-8')
+    return r
 
